7c8e51f45a24bf5992b81111b8643a5aa420e2ba
Shun Sakai <sorairolake@protonmail.ch>
2025-07-26 09:12:55 +0900

    chore: Initialize .NET project

---
+++ src/Sysexits/ExitCode.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-FileCopyrightText: 2025 Shun Sakai
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+namespace Sysexits;
+
+public class ExitCode
+{
+    public static ulong Add(ulong left, ulong right)
+    {
+        return left + right;
+    }
+}
---
+++ tests/Sysexits.Tests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿// SPDX-FileCopyrightText: 2025 Shun Sakai
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+namespace Sysexits.Tests;
+
+public class UnitTest
+{
+    [Fact]
+    public void Add()
+    {
+        var result = ExitCode.Add(2, 2);
+        Assert.Equal(4U, result);
+    }
+}
